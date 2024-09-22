62f37cf30f7bd82e73a7bf65f5ea20901d6265c2
Nabaraj bhusal <bhusalnabu1234@gmail.com>
2024-09-22 16:30:32 -0400

    fruitsales

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,3 @@
-# add your code here
+import pandas as pd
+fruit_sales = pd.DataFrame({'Apples': [35, 41], 'Bananas': [25,34]}, index=['2017 sales', '2018 sales'])
+fruit_sales.to_csv('fruit.csv', index=False)
